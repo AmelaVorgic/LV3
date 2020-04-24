210bdfe2a2aa71fead5dff698c3f3fe3f7143092
Amela Vorgic <amela.vorgic@student.ferit.hr>
2020-04-24 08:54:17 +0200

    .

---
+++ NotificationDirector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3
+{
+    class NotificationDirector
+    {
+        public ConsoleNotification CreateINFONotification(string author)
+        {
+
+            NotificationBuilder note = new NotificationBuilder();
+            note.SetAuthor(author);
+            note.SetTitle("title");
+            note.SetLevel(Category.INFO);
+            note.SetColor(ConsoleColor.Green);
+            note.SetText("Content");
+
+            return note.Build();
+        }
+
+        public ConsoleNotification CreateERRORNotification(string author)
+        {
+
+            NotificationBuilder note = new NotificationBuilder();
+            note.SetAuthor(author);
+            note.SetTitle("title");
+            note.SetLevel(Category.ERROR);
+            note.SetColor(ConsoleColor.White);
+            note.SetText("Content");
+
+            return note.Build();
+        }
+
+        public ConsoleNotification CreateALERTNotification(string author)
+        {
+
+            NotificationBuilder note = new NotificationBuilder();
+            note.SetAuthor(author);
+            note.SetTitle("title");
+            note.SetLevel(Category.ALERT);
+            note.SetColor(ConsoleColor.Red);
+            note.SetText("Content");
+
+            return note.Build();
+        }
+    }
+}
